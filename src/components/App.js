5dd7fe8b2737d5bd537ed4a7904e64537588e098
Yu Shi <yushiys95@gmail.com>
2021-04-10 00:24:01 -0400

    first bonus finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
-
 import hogs from "../porkers_data";
+import HogList from "./HogList";
+
 
 function App() {
+
+  const [isShowHogs, setIsShowHogs] = useState(true)
+
+  const handleHogsClick = () => {
+    setIsShowHogs((isShowHogs) => !isShowHogs)
+     console.log(isShowHogs)
+  }
+
   return (
     <div className="App">
       <Nav />
+      <button onClick={handleHogsClick}>{!isShowHogs ? "Hide" : "Show" } Hogs</button>
+      <HogList hogs={hogs} isShowHogs={isShowHogs}/>
     </div>
   );
 }
---
+++ src/components/FilterForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function FilterForm({ handleCheckBox, handleDropDown, checkBox, dropDown }){
+    return (
+        <form>
+            <input type="checkbox" checked={checkBox ? "true" : ""} onChange={handleCheckBox}/>
+
+            <select onChange={handleDropDown} value={dropDown}>
+                <option value="All">All</option>
+               <option value="Name">Name</option>
+                <option value="Weight">Weight</option>
+            </select>
+        </form>
+    )
+}
+
+export default FilterForm;
